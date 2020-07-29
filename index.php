56c79783c79ecb1ff5e39f9c61538d02c016afcc
iiadarsh <68966900+iiadarsh@users.noreply.github.com>
2020-07-30 02:05:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Welcome to OpenShift Online Developer Preview";
+echo "iiadarsh : Welcome to OpenShift Online Developer Preview";
 echo "<br>To test the database, hit the dbtest.php URL";
 
 
